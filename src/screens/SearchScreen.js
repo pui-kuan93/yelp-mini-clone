863107832b0dad9c58bf7d5e2b2502c65e7eea51
Ng Pui Kuan <missnpk93@gmail.com>
2019-10-04 00:04:36 +0800

    added stack navigator

---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const SearchScreen = () => {
+  return (
+    <View>
+      <Text>SearchScreen</Text>
+    </View>
+  );
+};
+
+const stylels = StyleSheet.create({});
+
+export default SearchScreen;
